d3939e859002a640d06e51aea50534aa8d8dab43
Edilainecsilva <edilainecsilva@outlook.com>
2024-01-25 16:51:27 -0300

    Atualizações diversas

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -47,9 +47,90 @@ export const data = [
 	{
 		"id":"6",
 		"bookCover":"/assets/img7.png",
-		"bookTitle":"teste",
-		"bookType":"Kiera Cass",
+		"bookTitle":"ESTILHAÇA-ME",
+		"bookType":"Ficção Juvenil",
 		"bookPrice":"29,99",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"7",
+		"bookCover":"/assets/img9.png",
+		"bookTitle":"INCENDEIA-ME",
+		"bookType":"Romance",
+		"bookPrice":"59,99",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"8",
+		"bookCover":"/assets/img10.png",
+		"bookTitle":"RESTAURA-ME",
+		"bookType":"Ficção",
+		"bookPrice":"29,99",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"9",
+		"bookCover":"assets/img11.png",
+		"bookTitle":"imagina-me",
+		"bookType":"Ficção Juvenil",
+		"bookPrice":"R$ 29,99",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"10",
+		"bookCover":"assets/img12.png",
+		"bookTitle":"desafia-me",
+		"bookType":"Ficção",
+		"bookPrice":"R$ 29,99",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"11",
+		"bookCover":"assets/img13.png",
+		"bookTitle":"liberta-me",
+		"bookType":"Ficção Juvenil | romance",
+		"bookPrice":"R$ 29,99",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"14",
+		"bookCover":"",
+		"bookTitle":"em breve",
+		"bookType":"romance",
+		"bookPrice":"R$ 0,00",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"12",
+		"bookCover":"assets/img8.png",
+		"bookTitle":"divergente",
+		"bookType":"Ficção Juvenil",
+		"bookPrice":"R$ 29,99",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"13",
+		"bookCover":"",
+		"bookTitle":"em breve",
+		"bookType":"Ficção Juvenil | romance",
+		"bookPrice":"R$ 0,00",
+		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
+		"bookAuthor":"Kiera Cass"
+	},
+	{
+		"id":"14",
+		"bookCover":"",
+		"bookTitle":"em breve",
+		"bookType":"Ficção Juvenil | romance",
+		"bookPrice":"R$ 0,00",
 		"bookDescriprion":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra mauris ac justo bibendum, sed dictum massa feugiat.",
 		"bookAuthor":"Kiera Cass"
 	},
